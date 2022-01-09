d8eff9b6491aed82b968d81ddb2e7f65119c23eb
KinderPinGuiin <jordan.teltel.2001@gmail.com>
2022-01-09 23:07:14 +0100

    Ajout de getters dans l'enum Tags

--- src/main/java/net/projet/schematicsinworld/parser/tags/Tags.java
+++ src/main/java/net/projet/schematicsinworld/parser/tags/Tags.java
@@ -1,6 +1,13 @@
 package net.projet.schematicsinworld.parser.tags;
 
+/**
+ * Liste des tags disponibles dans un fichier NBT. Les tags seront associés à
+ * leur classe.
+ *
+ * @see <a href="https://wiki.vg/NBT">Spécification des fichiers NBT</a>
+ */
 public enum Tags {
+
     TAG_END(null),
     TAG_BYTE(TagByte.class),
     TAG_SHORT(TagShort.class),
@@ -15,9 +22,37 @@ public enum Tags {
     TAG_INT_ARRAY(null),
     TAG_LONG_ARRAY(null);
 
-    private Class className;
+    private Class aClass;
 
     private Tags(Class aClass) {
-        this.className = aClass;
+        this.aClass = aClass;
     }
+
+    /*
+     * Requêtes
+     */
+
+    /**
+     * Renvoie le tag associée à l'identifiant ord.
+     *
+     * @param {int} L'ID du tag.
+     * @return {Tags} Le tag associé. Retourne null si aucun tag n'a été trouvé.
+     */
+    public Tags getTagByOrd(int ord) {
+        for (Tags tag : Tags.values()) {
+            if (tag.ordinal() == ord) {
+                return tag;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * @return {Class} La classe associée au tag.
+     */
+    public Class getTagClass() {
+        return this.aClass;
+    }
+
 }
