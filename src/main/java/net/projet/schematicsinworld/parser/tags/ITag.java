4b3c9b90f351104d4a8e2b856992281d2f06daef
KinderPinGuiin <jordan.teltel.2001@gmail.com>
2022-01-09 17:41:52 +0100

    Ajout de la classe abstraite Tag et de l'interface ITag

---
+++ src/main/java/net/projet/schematicsinworld/parser/tags/ITag.java
@@ -0,0 +1,23 @@
+package net.projet.schematicsinworld.parser.tags;
+
+/**
+ * Définit les différents types de données que l'on peut retrouver dans un
+ * fichier NBT.
+ */
+public interface ITag {
+
+    /**
+     * Renvoie la clé correspondante au tag.
+     *
+     * @return {String} La clé.
+     */
+    String getKey();
+
+    /**
+     * Renvoie la valeur correspondante au tag.
+     *
+     * @return {Object} La valeur.
+     */
+    Object getValue();
+
+}
--- src/main/java/net/projet/schematicsinworld/parser/tags/Tag.java
+++ src/main/java/net/projet/schematicsinworld/parser/tags/Tag.java
@@ -1,7 +1,33 @@
 package net.projet.schematicsinworld.parser.tags;
 
-public abstract class Tag {
+import net.projet.schematicsinworld.parser.utils.StringStream;
 
+public abstract class Tag implements ITag {
 
+    /*
+     * Attributs
+     */
+
+    protected String key;
+    protected Object value;
+
+    /*
+     * Requêtes
+     */
+
+    public abstract String getKey();
+    public abstract Object getValue();
+
+    /*
+     * Commandes
+     */
+
+    /**
+     * Lit le bon nombre de données sur la chaîne contenu dans buffer afin de
+     * définir les différentes données du tag.
+     *
+     * @param {StringStream} Le buffer contenant les prochaines données à lire.
+     */
+    protected abstract void parseBuffer(StringStream buffer);
 
 }
