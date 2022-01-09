709f08218cb6889b131f15ff3c85333c36642cd5
KinderPinGuiin <jordan.teltel.2001@gmail.com>
2022-01-09 21:32:12 +0100

    Ajout d'une énumération TagID

---
+++ src/main/java/net/projet/schematicsinworld/parser/tags/TagID.java
@@ -0,0 +1,16 @@
+package net.projet.schematicsinworld.parser.tags;
+
+public enum TagID {
+    TAG_END,
+    TAG_BYTE,
+    TAG_SHORT,
+    TAG_INT,
+    TAG_LONG,
+    TAG_FLOAT,
+    TAG_DOUBLE,
+    TAG_BYTE_ARRAY,
+    TAG_LIST,
+    TAG_COMPOUND,
+    TAG_INT_ARRAY,
+    TAG_LONG_ARRAY
+}
