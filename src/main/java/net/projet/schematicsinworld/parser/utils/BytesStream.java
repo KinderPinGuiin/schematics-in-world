10fd2007d53d3c38e80457cff2bb95331bd47c77
KinderPinGuiin <jordan.teltel.2001@gmail.com>
2022-01-15 16:43:23 +0100

    BytesStream peut désormais écrire des données sur son flux

--- src/main/java/net/projet/schematicsinworld/parser/utils/BytesStream.java
+++ src/main/java/net/projet/schematicsinworld/parser/utils/BytesStream.java
@@ -9,10 +9,26 @@ import java.util.Arrays;
  */
 public class BytesStream {
 
+    /*
+     * Constantes
+     */
+
+    public static final char READ_MODE = 'r';
+    public static final char WRITE_MODE = 'w';
+
+    /*
+     * Attributs
+     */
+
+    private char mode;
     private int currIndex;
     private ArrayList<Byte> bytes;
 
-    public BytesStream(byte[] ba, int i) {
+    /*
+     * Constructeurs
+     */
+
+    public BytesStream(byte[] ba, int i, char mode) {
         if (ba == null) {
             throw new AssertionError("string is null");
         }
@@ -20,15 +36,16 @@ public class BytesStream {
             throw new AssertionError("invalid index");
         }
         this.fillList(ba);
-        currIndex = i;
+        this.currIndex = i;
+        this.mode = mode;
     }
 
     public BytesStream(byte[] b) {
-        this(b, 0);
+        this(b, 0 , READ_MODE);
     }
 
-    public BytesStream() {
-        this(new byte[] {}, 0);
+    public BytesStream(char mode) {
+        this(new byte[] {}, 0, mode);
     }
 
     /**
@@ -52,7 +69,8 @@ public class BytesStream {
     }
 
     /**
-     * Définit le nouveau contenu du flux et replace le curseur à index.
+     * Définit le nouveau contenu du flux et replace le curseur à
+     * ou à bytes.length si le mode du flux est WRITE_MODE.
      *
      * @param bytes Le tableau d'octets contenant le nouveau flux.
      * @param index Le nouveau curseur.
@@ -62,16 +80,17 @@ public class BytesStream {
             throw new AssertionError("La chaîne ne doit pas être nulle");
         }
         this.fillList(bytes);
-        this.currIndex = index;
+        this.currIndex = this.mode == READ_MODE ? index : bytes.length;
     }
 
     /**
-     * Définit le nouveau contenu du flux et replace le curseur à 0.
+     * Définit le nouveau contenu du flux et replace le curseur à 0 ou à
+     * bytes.length si le mode du flux est WRITE_MODE.
      *
      * @param bytes Le tableau d'octets contenant le nouveau flux.
      */
     public void setBytes(byte[] bytes) {
-        this.setBytes(bytes, 0);
+        this.setBytes(bytes, this.mode == READ_MODE ? 0 : bytes.length);
     }
 
     /**
@@ -83,6 +102,9 @@ public class BytesStream {
     public byte[] read(int nBytes) {
         if (this.currIndex + nBytes > this.bytes.size()) {
             throw new AssertionError("Nombre de bytes à lire trop grand");
+        }
+        if (this.mode == WRITE_MODE) {
+            throw new AssertionError("Le flux est en mode écriture");
         }
         byte[] b = new byte[nBytes];
         for (int i = 0; i < nBytes; ++i) {
@@ -98,7 +120,12 @@ public class BytesStream {
      * @param bytes Les octets à écrire
      */
     public void write(byte[] bytes) {
-
+        if (this.mode == READ_MODE) {
+            throw new AssertionError("Le flux est en mode lecture");
+        }
+        for (int i = 0; i < bytes.length; ++i) {
+            this.bytes.add(bytes[i]);
+        }
     }
 
     /*
